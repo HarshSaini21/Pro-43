71b0e82855ce15d8f8f2f544655b6b94505c7521
HarshSaini21 <84327723+HarshSaini21@users.noreply.github.com>
2021-07-31 14:44:55 +0530

    Add files via upload

---
+++ Pro-43 Treasure Hunt/Treasure-Hunt-Template-main/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues(){
+    fill("white");
+    textSize(15);
+    text("R E V I A B L E A", 100, 50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant!!", 100, 70);
+
+    fill("white");
+    textSize(15);
+    text("F I O T U N C N", 700, 150);
+    fill("lightblue");
+    text("Hint: Performs a particular task!!", 700, 170);
+
+    fill("white");
+    textSize(15);
+    text("D T B A A S E A", 100, 330);
+    fill("lightblue");
+    text("Hint: Stores all information", 100, 350);
+
+}
---
+++ Pro-43 Treasure Hunt/Treasure-Hunt-Template-main/js/System.js
@@ -0,0 +1,18 @@
+class System{
+    constructor(){
+
+    }
+
+    authenticate(actualCode, enteredCode){
+        textSize(50);
+        fill(0);
+        text(code, 300,300);
+
+        if(actualCode===enteredCode.toUpperCase()){
+            return true
+        }else{
+            return false
+        }
+
+    }
+}
